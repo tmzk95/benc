32c6d89c29e7573c281f8dde83e4712eef52fc72
Dominik Tomczyk <tmzk95@gmail.com>
2021-12-14 19:57:22 +0100

    added logo link to home

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const CARD_IMAGES = [
-  { src: "images/cards/card1.png", type: "green" },
+  { src: "images/cards/card1.png", type: "red" },
   { src: "images/cards/card2.png", type: "gold" },
   { src: "images/cards/card3.png", type: "green" },
   { src: "images/cards/card4.png", type: "green" },
   { src: "images/cards/card5.png", type: "gold" },
   { src: "images/cards/card6.png", type: "gold" },
 ];
+
+const FLAG_COLORS = ["red", "green", "blue", "black", "silver", "pink"];
 
 function startNewGame() {
   const selectedCard = drawCard();
@@ -22,21 +24,36 @@ function startNewGame() {
 
   const buttonContainer = document.getElementsByClassName("button-container");
   buttonContainer[0].classList.add("hidden");
+
+  displayRandomFlagColor();
+
+  const lines = document.getElementsByClassName("stats-line");
+  Array.from(lines).forEach((line, index) => {
+    setTimeout(() => {
+      line.classList.remove("opacity-off");
+    }, index * 1000 + 3000);
+  });
 }
 
 function displayGradientContainer(type) {
   if (type === "gold") {
-    const gradientContainer = document.getElementsByClassName(
-      "gradient-container-gold"
-    );
+    const gradientContainer = document.getElementsByClassName("gradient-gold");
     gradientContainer[0].classList.remove("opacity-off");
   }
   if (type === "green") {
-    const gradientContainer = document.getElementsByClassName(
-      "gradient-container-green"
-    );
+    const gradientContainer = document.getElementsByClassName("gradient-green");
     gradientContainer[0].classList.remove("opacity-off");
   }
+  if (type === "red") {
+    const gradientContainer = document.getElementsByClassName("gradient-red");
+    gradientContainer[0].classList.remove("opacity-off");
+  }
+}
+
+function displayRandomFlagColor() {
+  let index = Math.round(Math.random() * (FLAG_COLORS.length - 1));
+  const flag = document.getElementsByClassName("stats-flag");
+  flag[0].classList.add(FLAG_COLORS[index]);
 }
 
 function drawCard() {
